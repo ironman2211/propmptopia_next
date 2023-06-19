f63601ab65616c2c2a88b7d36613cd5b35feb207
Brahma behera <beherabrahmananda77@gmail.com>
2023-06-20 00:47:17 +0530

    created next js application and imolemented NextAuth

---
+++ components/Nav.jsx
@@ -0,0 +1,134 @@
+"use client"
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import { useState, useEffect } from 'react'
+import { signIn, signOut, useSession, getProviders } from 'next-auth/react'
+
+const Nav = () => {
+  const {data:session} =useSession()
+  const [toggleDropdown, setToggleDropdown] = useState(false)
+  const [providers, setProvider] = useState(null)
+  useEffect(() => {
+    const setProviders = async () => {
+      const response = await getProviders()
+      setProvider(response)
+    }
+    setProviders()
+  })
+  // console.log(session)
+  return (
+    <nav className='flex-between w-full mb-16 pt-3'>
+      <Link href='/' className='flex gap-2 flex-center'>
+        <Image
+          src='/assets/images/logo.svg'
+          alt='logo'
+          width={30}
+          height={30}
+          className='object-contain'
+        />
+        <p className='logo_text'>Promptopia</p>
+      </Link>
+      <div className="sm:flex hidden">
+
+        {
+          session?.user ? <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt">
+              <button className='black_btn'>
+                Create Post
+              </button>
+            </Link>
+            <button className='black_btn' onClick={signOut}>
+              Sign out
+            </button>
+            <Link href="/profile">
+              <Image
+                  src={session?.user.image}
+                  width={37}
+                  height={37}
+                  className='rounded-full'
+                  alt='profile'
+
+              />
+            </Link>
+          </div> : (<>
+            {
+              providers && Object.values(providers).map((provider) => (
+                <button
+                  type='button'
+                  key={provider.name}
+                  className='black_btn'
+                  onClick={() => signIn(provider.id)}
+                >
+                  Sign In
+                </button>
+              ))
+            }
+          </>)
+        }
+
+      </div>
+      <div className='sm:hidden flex relative'>
+        {session?.user? (
+          <div className='flex'>
+            <Image
+          src={session?.user.image}
+          width={37}
+          height={37}
+          className='rounded-full'
+              alt='profile'
+              onClick={() => setToggleDropdown(!toggleDropdown)}
+            />
+
+            {toggleDropdown && (
+              <div className='dropdown'>
+                <Link
+                  href='/profile'
+                  className='dropdown_link'
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  My Profile
+                </Link>
+                <Link
+                  href='/create-prompt'
+                  className='dropdown_link'
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  Create Prompt
+                </Link>
+                <button
+                  type='button'
+                  onClick={() => {
+                    setToggleDropdown(false);
+                    signOut();
+                  }}
+                  className='mt-5 w-full black_btn'
+                >
+                  Sign Out
+                </button>
+              </div>
+            )}
+          </div>
+        ) : (
+          <>  
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type='button'
+                  key={provider.name}
+                  onClick={() => {
+                    signIn(provider.id);
+                  }}
+                  className='black_btn'
+                >
+                  Sign in
+                </button>
+              ))}
+          </>
+        )}
+      </div>
+    </nav>
+  )
+}
+
+export default Nav
---
+++ utils/database.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+export const connectToDB = async () => {
+    let isConnected = false;
+
+    mongoose.set('strictQuery', true);
+    if(isConnected) {
+        console.log('=> using existing database connection');
+        return ;
+    }
+    try {
+  mongoose.connect(process.env.MONGODB_URI, {
+    dbName: "sharedPrompts",
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    });
+    isConnected = true;
+    console.log('=> using new database connection');
+
+
+    } catch (error) {
+        console.log('=> error while connecting with database');
+
+    }
+
+};
